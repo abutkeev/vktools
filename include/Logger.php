e769a0bd3700850b2e28b719d8d4fa50fca5b4a5
Alexey V. Butkeev <abutkeev@abutkeev.ru>
2015-09-29 13:54:42 +0300

    methods for temporary debug in logger

--- include/Logger.php
+++ include/Logger.php
@@ -2,6 +2,9 @@
   final class Logger {
     static private $instance;
     static public $debug = false;
+
+    static private $old_debug = false;
+
     private function __construct($ident = 'Logger', $option = LOG_PERROR, $facility = LOG_LOCAL0) {
       openlog($ident, $option, $facility);
       self::$instance = $this;
@@ -12,11 +15,22 @@
         new self($ident, $option, $facility);
       }
     }
+
     static function log($priority, $message) {
       if (!isset(self::$instance))
         new self();
       if ($priority == LOG_DEBUG && self::$debug)
         syslog($priority, $message);
+    }
+
+    static function temporary_debug_on() {
+      self::$old_debug = self::$debug;
+
+      self::$debug = true;
+    }
+
+    static function temporary_debug_off() {
+      self::$debug = self::$old_debug;
     }
   }
 ?>
