b5875701704c8ba00f35cd1093ada780c59308a6
Alexey V. Butkeev <abutkeev@abutkeev.ru>
2015-10-12 17:11:42 +0300

    debug commandlime parameter

--- save_online.php
+++ save_online.php
@@ -2,6 +2,10 @@
 
 require_once('include/vkTools.php');
 Logger::init('save_online', 0);
+
+if ($argc == 2 && $argv[1] == 'debug')
+  Logger::debug(true);
+
 $tools = new vkTools();
 $tools->save_all_online();
 $tools->merge_sessions(12*60);
